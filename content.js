5a5dc33e70926d0acacd69585efb6ac138962831
alexander <alexander@klippel.se>
2024-09-27 13:14:27 +0200

    Delete content.js

--- content.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.sync.get('darkThemeEnabled', (data) => {
-    if (data.darkThemeEnabled) {
-      chrome.scripting.insertCSS({
-        target: {tabId: chrome.tabs.getCurrent().id},
-        files: ["style.css"]
-      });
-    }
-  });
-  
